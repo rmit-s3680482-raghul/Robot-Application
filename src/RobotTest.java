c0114a32213704d53cbadcdf4f481c5f90c24464
Raghul Karthik <s3680482@student.rmit.edu.au>
2020-02-02 23:49:46 +1100

    Add comments to Robot Test.

--- src/RobotTest.java
+++ src/RobotTest.java
@@ -9,6 +9,7 @@ public class RobotTest {
 	
 	@Test
 	void testRobotController() {
+		// test initilization of constructor
 		RobotController robot = new RobotController(0, 0, 0, 0);
 		assertEquals(robot.getCoordinateX(), 0);
 		assertEquals(robot.getCoordinateY(), 0);
@@ -18,11 +19,11 @@ public class RobotTest {
 	
 	@Test
 	void testValidation() {
-		String[] userCommand1 = { "k6", "w2" }; 
-		String[] userCommand2 = { "f1", "b2", "l1", "l1", "b1", "f4" }; 
-		String[] userCommand3 = { "f9", "j5" }; 
-		String[] userCommand4 = { "s1", "s-2" };
-		String[] userCommand5 = { "q1", "22" }; 
+		String[] userCommand1 = { "k6", "w2" }; // invalid Commands
+		String[] userCommand2 = { "f1", "b2", "l1", "l1", "b1", "f4" }; // valid user input
+		String[] userCommand3 = { "f9", "j5" }; // invalid Commands
+		String[] userCommand4 = { "s1", "s-2" }; // invalid Commands
+		String[] userCommand5 = { "q1", "22" }; // invalid Commands
 		boolean IsValidCommand1 = Robot.Validation(userCommand1);
 		boolean IsValidCommand2 = Robot.Validation(userCommand2);
 		boolean IsValidCommand3 = Robot.Validation(userCommand3);
@@ -36,11 +37,13 @@ public class RobotTest {
 	}
 	@Test
 	void testRobotApplication() {
+		// initialize the constructor
 		RobotController robotController = new RobotController(0, 0, 0, 0);
 		String[] testArray = { "f1", "r1", "b2", "l1", "b3"};
 		boolean testCommandValid = Robot.Validation(testArray);
 		assertEquals(true, testCommandValid);
 		Robot.robotMove(testArray, robotController);
+		// Test for valid input for coorinates, direction and minimum distance from the initial position.
 		assertEquals(robotController.getCoordinateX(), -2);
 		assertEquals(robotController.getCoordinateY(), -2);
 		assertEquals(robotController.getUnitFrominitialPosition(), 4);
